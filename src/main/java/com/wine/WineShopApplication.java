d0b9e866aae5197751d077b64912b574a11db342
Thiago Oliveira <thiago.oliveira4_ext@michelin.com>
2021-02-25 21:56:39 -0300

    Iniciando projeto

---
+++ src/main/java/com/wine/WineShopApplication.java
@@ -0,0 +1,13 @@
+package com.wine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WineShopApplication.class, args);
+	}
+
+}
